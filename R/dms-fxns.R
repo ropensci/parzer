d599803fce017c32255af7819198af1d83a40ac1
Scott Chamberlain <myrmecocystus@gmail.com>
2019-08-24 07:48:30 -0700

    fix examples in dms manual file

--- R/dms-fxns.R
+++ R/dms-fxns.R
@@ -8,9 +8,7 @@ dms_helper <- function(lat = NULL, lon = NULL) {
 
 #' extract degree, minutes, and seconds
 #'
-#' @export
 #' @name dms
-#' @aliases degrees minutes seconds
 #' @param lat,lon (numeric/integer/character) one or more latitude or
 #' longitude values. values are internally validated. only one of
 #' lat or lon accepted
@@ -28,9 +26,9 @@ dms_helper <- function(lat = NULL, lon = NULL) {
 #' pz_minute(lon = 178.23423)
 #' pz_second(lon = 178.23423)
 #'
-#' pz_degree(lat = c(45.23323, "40:25:6N", "40° 25´ 5.994\" N"))
-#' pz_minute(lat = c(45.23323, "40:25:6N", "40° 25´ 5.994\" N"))
-#' pz_second(lat = c(45.23323, "40:25:6N", "40° 25´ 5.994\" N"))
+#' pz_degree(lat = c(45.23323, "40:25:6N", "40° 25´ 5.994 N"))
+#' pz_minute(lat = c(45.23323, "40:25:6N", "40° 25´ 5.994 N"))
+#' pz_second(lat = c(45.23323, "40:25:6N", "40° 25´ 5.994 N"))
 #'
 #' # invalid
 #' pz_degree(445.23323)
@@ -41,6 +39,10 @@ dms_helper <- function(lat = NULL, lon = NULL) {
 #' d(31) + m(44) + s(59)
 #' d(-121) + m(1) + s(33)
 #' unclass(d(31) + m(44) + s(59))
+NULL
+
+#' @export
+#' @rdname dms
 pz_degree <- function(lat = NULL, lon = NULL) {
   dms_helper(lat, lon)$deg
 }
