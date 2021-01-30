d7dc10eb14cd93eb877aaaafdd3bd2388199735f
Hiroaki Yutani <yutani.ini@gmail.com>
2021-01-30 10:27:33 +0900

    Add a failing test

---
+++ tests/testthat/test-mbcs-locale.R
@@ -0,0 +1,31 @@
+# std::regex has a bug on non-UTF-8 MBCS locale. We use Japanese locale for
+# testing, but it's not available on all machines (GitHub Actions' Windows
+# runner has it installed, though).
+skip_if_japanese_locale_unavailable <- function() {
+  loc <- Sys.getlocale("LC_COLLATE")
+  on.exit(Sys.setlocale("LC_COLLATE", loc))
+
+  tryCatch(
+    Sys.setlocale("LC_COLLATE", "Japanese"),
+    warning = function(e) {
+      skip("Japanese locale is not available")
+    }
+  )
+}
+
+test_that("parse_lat() works on Japanese locale", {
+  skip_if_japanese_locale_unavailable()
+  skip_if_not_installed("callr")
+  skip_if_not_installed("pkgload")
+
+  # Since parse_lat("10") might take very long time if it hits the std::regex's
+  # bug, so we need to call it as an external process so that we can set timeout.
+  test_func <- function() {
+    pkgload::load_all(".")
+    withr::with_locale(
+      c(LC_COLLATE = "Japanese"),
+      parse_lat("10")
+    )
+  }
+  expect_error(callr::r_safe(test_func, timeout = 10), NA)
+})
