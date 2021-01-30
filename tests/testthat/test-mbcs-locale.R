b838cadf89032261765dbd7f9cbc9b66df6a5f96
Hiroaki Yutani <yutani.ini@gmail.com>
2021-01-30 12:50:36 +0900

    Build package beforehand

--- tests/testthat/test-mbcs-locale.R
+++ tests/testthat/test-mbcs-locale.R
@@ -21,11 +21,17 @@ test_that("parse_lat() works on Japanese locale", {
   # Since parse_lat("10") might take very long time if it hits the std::regex's
   # bug, so we need to call it as an external process so that we can set timeout.
   test_func <- function() {
-    pkgload::load_all(".", compile = FALSE)
+    pkgload::load_all(".", recompile = FALSE)
     withr::with_locale(
       c(LC_COLLATE = "Japanese"),
       parse_lat("10")
     )
   }
+
+  # On Windows, `pkgload::load_all()` requires compilation (I don't understand why).
+  # But, we cannot let it happen in `test_func()` otherwise it easily times out.
+  # So, do it beforehand.
+  pkgbuild::compile_dll(".", force = FALSE)
+
   expect_error(callr::r_safe(test_func, timeout = 10), NA)
 })
