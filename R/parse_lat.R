af6aa0235794b83d3a821d019d4efdec23f4ba15
AlbanSagouis <alban.sagouis@idiv.de>
2022-10-18 18:25:26 +0200

    Revert "adding a warning to parse_lon and parse_lat" tests were not updated and fail, not the good time This reverts commit e01381237d663b51d30280a53a8a1d4cecb6ad09.

--- R/parse_lat.R
+++ R/parse_lat.R
@@ -55,10 +55,7 @@
 #' }
 parse_lat <- function(lat, format = NULL) {
   assert(lat, c("numeric", "integer", "character"))
-  # assert(format, "character")
-  if (!is.null(format)) warning("At the moment, format is not taken into account by the function.")
-
-  return(
-    pz_parse_lat(scrub(lat))
-  )
+  assert(format, "character")
+  lat <- scrub(lat)
+  if (is.null(format)) pz_parse_lat(lat) else stop_form()
 }
--- R/parse_lon.R
+++ R/parse_lon.R
@@ -58,10 +58,8 @@
 #' }
 parse_lon <- function(lon, format = NULL) {
   assert(lon, c("numeric", "integer", "character"))
-  # assert(format, "character")
-  if (!is.null(format)) warning("At the moment, format is not taken into account by the function.")
+  assert(format, "character")
 
-  return(
-    pz_parse_lon(scrub(lon))
-  )
+  lon <- scrub(lon)
+  if (is.null(format)) pz_parse_lon(lon) else stop_form()
 }
